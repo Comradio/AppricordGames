ff00d22d54a4b8e53b3e864c6028951379101269
comradise <comradise@mail.ru>
2022-02-20 14:55:51 +0400

    Create try

---
+++ AprikordGames/AprikordGames/Models/GameGenre.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace AprikordGames.Models
+{
+    public class GameGenre
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Genre { get; set; }
+        [JsonIgnore]
+        public ICollection<Game>? Games { get; set; }
+    }
+}
